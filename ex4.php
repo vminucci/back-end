2b5089811e169b8c0b24d9c08d803e9b4bb32f06
Victor Matos Minucci Volpe <victor.volpe@aluno.senai.br>
2026-09-09 08:53:31 -0300

    lista 8

---
+++ ex4.php
@@ -0,0 +1,10 @@
+<?php
+
+$num = 7;
+
+for($i = 1; $i <= 10; $i++){
+    $resultado = $num * $i;
+
+    echo "$num x $i = $resultado <br>";
+}
+?>
---
+++ ex5.php
@@ -0,0 +1,6 @@
+<?php
+
+for($numero = 10; $numero >= 1; $numero--){
+    echo "$numero <br>";
+}
+echo "Game Over";
